6d99af85b18d5aaea010652ff5e40afbf7899b79
Pratyush Shukla <ps4534@nyu.edu>
2022-01-29 23:54:39 -0500

    Reconfiguring folders
    Deleted all files

--- Topic 1/BinomialTreeModel/Option01/Option01.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-namespace fre {
-	// pricing European option
-	double PriceByCRR(double S0, double U, double D, double R, int N, double K);
-
-	// computing call payoff
-	double CallPayoff(double z, double K);
-}
